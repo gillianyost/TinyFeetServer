0a31a69c1a8a1aa03bc44c329fc26006c08c64ba
Gillian Yost <gillianyost@gmail.com>
2021-03-04 20:44:42 -0700

    Recommendation filter working but not selecting proper columns or column names from 30 percent

--- server/survey/forms.py
+++ server/survey/forms.py
@@ -3,11 +3,14 @@ from wtforms import SelectField, SubmitField, BooleanField
 
 
 class RecForm(FlaskForm):
+    allSol = BooleanField('All Recommendations')
     equity = BooleanField('Equity')
+    econSus = BooleanField('Economic Sustainability')
     envQuality = BooleanField('Local Environmental Quality')
     healthSafety = BooleanField('Enhancing Public Health and Safety')
     resilience = BooleanField('Building Community Resilience')
 
+    allSec = BooleanField('All Sectors')
     cementMan = BooleanField('Cement And Manufacturing')
     waste = BooleanField('Waste')
     electricity = BooleanField('Electricity')

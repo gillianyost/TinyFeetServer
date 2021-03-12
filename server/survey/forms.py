5df7b4ca4fe3e11382c314ab71fc78105460d996
Gillian Yost <gillianyost@gmail.com>
2021-03-11 23:37:36 -0700

    Recommendation Filters Working Fully, Resource Page Link and Files Populated, Temp Feedback Tab added with survey

--- server/survey/forms.py
+++ server/survey/forms.py
@@ -3,18 +3,19 @@ from wtforms import SelectField, SubmitField, BooleanField
 
 
 class RecForm(FlaskForm):
-    allSol = BooleanField('All Recommendations')
+    allSol = BooleanField('All Recommendations', default=True)
     equity = BooleanField('Equity')
     econSus = BooleanField('Economic Sustainability')
     envQuality = BooleanField('Local Environmental Quality')
     healthSafety = BooleanField('Enhancing Public Health and Safety')
     resilience = BooleanField('Building Community Resilience')
 
-    allSec = BooleanField('All Sectors')
+    allSec = BooleanField('All Sectors', default=True)
     cementMan = BooleanField('Cement And Manufacturing')
-    waste = BooleanField('Waste')
+    waste = BooleanField('Waste and Land Management')
     electricity = BooleanField('Electricity')
+    energy = BooleanField('Energy')
     naturalGas = BooleanField('Natural Gas')
-    transportation = BooleanField('Transportation')
+    transportation = BooleanField('Vechicles and Transportation')
 
     submit = SubmitField()
--- server/survey/views.py
+++ server/survey/views.py
@@ -3,7 +3,7 @@ from server import db
 from sqlalchemy import distinct, inspect
 import collections
 
-from server.models import Cement_and_manufacturing, Electricity, Natural_gas, Otis_transportation, Waste, Aviation, Zip_pop, Zip_data, Solutions
+from server.models import Cement_and_manufacturing, Electricity, Natural_gas, Otis_transportation, Waste, Aviation, Zip_pop, Zip_data, Solutions, Solutions_vech_tran, Solutions_waste, Solutions_vech_tran_energy, Solutions_energy, Solutions_energy_waste, Solutions_waste_vech_tran
 from server.survey.forms import RecForm
 from server.emissions.views import object_as_dict, coalesce
 import collections
@@ -20,43 +20,85 @@ def recommendations():
     tableData = []
     if request.method == 'POST':
        
-        if form.allSol.data:
-            # query = Solutions.query(Solutions.solution_description, \
-            # Solutions.section, \
-            # Solutions.subsection, \
-            # Solutions.ghg_reduction_potential) # Cannot get to work TO DO
-
-            # columnNames = Solutions.__tablename__.columns.keys()  # Cannot get to work TO DO
+        if form.allSol.data & form.allSec.data:
             query = Solutions.query.all()
-        else: 
-            # query = Solutions.query(Solutions.solution_description, \
-            # Solutions.section, \
-            # Solutions.subsection, \
-            # Solutions.ghg_reduction_potential)\
-            # .filter((Solutions.equity & form.equity.data) | \
-            # (Solutions.economic_sustainability & form.econSus.data) | \
-            # (Solutions.local_environmental_quality & form.envQuality.data) | \
-            # (Solutions.enhances_public_safety & form.healthSafety.data) | \
-            # (Solutions.builds_resilience & form.resilience.data)) # Cannot get to work TO DO
+            #columnNames = Solutions.__tablename__.columns.keys()
+            for row in query:
+                d = object_as_dict(row)
+                tableData.append(d.values())
 
-            # columnNames = Solutions.__tablename__.columns.keys()  # Cannot get to work TO DO
-            query = Solutions.query.filter((Solutions.equity & form.equity.data) | \
-            (Solutions.economic_sustainability & form.econSus.data) | \
-            (Solutions.local_environmental_quality & form.envQuality.data) | \
-            (Solutions.enhances_public_safety & form.healthSafety.data) | \
-            (Solutions.builds_resilience & form.resilience.data))
-
-        for row in query:
-            d = object_as_dict(row)
-            tableData.append(d.values())
-    
-    # if request.method == 'POST':
-    #     if form.equity.data:
-    #         columnNames = recommendations.__table__.columns.keys()
-    #         query = recommendations.all()
-    
-    # tableData = []
-    # d = object_as_dict(row)
-    # tableData.append(d.values())
+        elif form.allSol.data == False:
+            if form.allSec.data == True:
+                query = Solutions.query.filter((Solutions.equity & form.equity.data) | \
+                (Solutions.economic_sustainability & form.econSus.data) | \
+                (Solutions.local_environmental_quality & form.envQuality.data) | \
+                (Solutions.enhances_public_safety & form.healthSafety.data) | \
+                (Solutions.builds_resilience & form.resilience.data))
+                #columnNames = Solutions.__tablename__.columns.keys()
+                for row in query:
+                    d = object_as_dict(row)
+                    tableData.append(d.values())
+            elif form.allSec.data == False:
+                if form.transportation.data == True:
+                    if form.energy.data == False: 
+                        if form.waste.data == False:
+                            query = Solutions_vech_tran.query.filter((Solutions_vech_tran.equity & form.equity.data) | \
+                            (Solutions_vech_tran.economic_sustainability & form.econSus.data) | \
+                            (Solutions_vech_tran.local_environmental_quality & form.envQuality.data) | \
+                            (Solutions_vech_tran.enhances_public_safety & form.healthSafety.data) | \
+                            (Solutions_vech_tran.builds_resilience & form.resilience.data))
+                if form.transportation.data == True:
+                    if form.energy.data == True:
+                        if form.waste.data == False:
+                            query = Solutions_vech_tran_energy.query.filter((Solutions_vech_tran_energy.equity & form.equity.data) | \
+                            (Solutions_vech_tran_energy.economic_sustainability & form.econSus.data) | \
+                            (Solutions_vech_tran_energy.local_environmental_quality & form.envQuality.data) | \
+                            (Solutions_vech_tran_energy.enhances_public_safety & form.healthSafety.data) | \
+                            (Solutions_vech_tran_energy.builds_resilience & form.resilience.data))
+                if form.transportation.data == True:
+                    if form.energy.data == False:
+                        if form.waste.data == True:
+                            query = Solutions_waste_vech_tran.query.filter((Solutions_waste_vech_tran.equity & form.equity.data) | \
+                            (Solutions_waste_vech_tran.economic_sustainability & form.econSus.data) | \
+                            (Solutions_waste_vech_tran.local_environmental_quality & form.envQuality.data) | \
+                            (Solutions_waste_vech_tran.enhances_public_safety & form.healthSafety.data) | \
+                            (Solutions_waste_vech_tran.builds_resilience & form.resilience.data))
+                if form.transportation.data == False:
+                    if form.energy.data == True:
+                        if form.waste.data == False:
+                            query = Solutions_energy.query.filter((Solutions_energy.equity & form.equity.data) | \
+                            (Solutions_energy.economic_sustainability & form.econSus.data) | \
+                            (Solutions_energy.local_environmental_quality & form.envQuality.data) | \
+                            (Solutions_energy.enhances_public_safety & form.healthSafety.data) | \
+                            (Solutions_energy.builds_resilience & form.resilience.data))
+                if form.transportation.data == False:
+                    if form.energy.data == True:
+                        if form.waste.data == True:
+                            query = Solutions_energy_waste.query.filter((Solutions_energy_waste.equity & form.equity.data) | \
+                            (Solutions_energy_waste.economic_sustainability & form.econSus.data) | \
+                            (Solutions_energy_waste.local_environmental_quality & form.envQuality.data) | \
+                            (Solutions_energy_waste.enhances_public_safety & form.healthSafety.data) | \
+                            (Solutions_energy_waste.builds_resilience & form.resilience.data))
+                if form.transportation.data == False:
+                    if form.energy.data == False:
+                        if form.waste.data == True:
+                            query = Solutions_waste.query.filter((Solutions_waste.equity & form.equity.data) | \
+                            (Solutions_waste.economic_sustainability & form.econSus.data) | \
+                            (Solutions_waste.local_environmental_quality & form.envQuality.data) | \
+                            (Solutions_waste.enhances_public_safety & form.healthSafety.data) | \
+                            (Solutions_waste.builds_resilience & form.resilience.data))
+                #columnNames = Solutions.__tablename__.columns.keys()
+                for row in query:
+                    d = object_as_dict(row)
+                    tableData.append(d.values())
+        elif form.allSec.data == False:
+            if form.allSol.data == True:
+                query = Solutions.query.filter((Solutions.vech_tran & form.transportation.data) | \
+                (Solutions.energy & form.energy.data) | \
+                (Solutions.waste & form.waste.data))
+                #columnNames = Solutions.__tablename__.columns.keys()
+                for row in query:
+                    d = object_as_dict(row)
+                    tableData.append(d.values())
 
     return render_template('/mainPages/recommendations.html', form=form, columnNames=columnNames, tableData=tableData)
